8524dc28fc0364faf2ea319d076a2899c72ebba8
unknown <libny.limas@ujcv.edu.hn>
2014-02-14 00:31:48 -0600

    agrege los cuatro numeros

--- src/proyecto1/proyecto1.cpp
+++ src/proyecto1/proyecto1.cpp
@@ -1,6 +1,28 @@
+#include <stdlib.h>    
+#include <time.h> 
 #include <stdio.h>
 
+
 int main ()
+	
 {
-	printf ("hola libny");
+	printf ("lansa numeros ");
+	printf("lanse el primer numero ");
+	int  a,b,c,d;
+	/* inicialize  la semilla del  random : */
+  srand (time(NULL));
+
+  /*genere un numero entre  0 y 9: */
+     a = rand() % 9 + 0;
+     b = rand() % 9 + 0;
+	 c= rand() % 9 + 0;
+	 d= rand() % 9 + 0;
+
+
+
+return 0;
+
 }
+
+
+
