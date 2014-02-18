2b0999dce6f2ff8074d57b3041f97d1ac3b81fb0
unknown <libny.limas@ujcv.edu.hn>
2014-02-17 23:36:30 -0600

    agrege las funcion lanzarnumeros

--- src/proyecto1/proyecto1.cpp
+++ src/proyecto1/proyecto1.cpp
@@ -1,28 +1,54 @@
 #include <stdlib.h>    
 #include <time.h> 
 #include <stdio.h>
+#include <iostream>
 
+using namespace std;
+int lanzarnumero();
 
 int main ()
 	
 {
-	printf ("lansa numeros ");
-	printf("lanse el primer numero ");
+	
+	cout<<("lanza numeros ")<<endl;
+	cout<<("lanse el primer numero ")<<endl;
 	int  a,b,c,d;
 	/* inicialize  la semilla del  random : */
   srand (time(NULL));
 
   /*genere un numero entre  0 y 9: */
-     a = rand() % 9 + 0;
-     b = rand() % 9 + 0;
-	 c= rand() % 9 + 0;
-	 d= rand() % 9 + 0;
+     a = lanzarnumero();
+     b = lanzarnumero();
+	 c = lanzarnumero();
+	 d = lanzarnumero();
+    
+
+	 
+	
+	 if(a==b==c==d)
+	 {
+		 cout<<"usted a ganado :)";
+	 }
+	 else 
+	 {
+		 cout<<"usted a perdido :(";
+
+	 }
 
 
-
-return 0;
+	getchar();
+	return 0;
 
 }
+
+int lanzarnumero()  
+{	
+	int numero; 
+	numero=rand() % 9 + 0;
+	cout<<" el  numero es: "<<numero<<endl;
+	return numero;
+}
+
 
 
 
