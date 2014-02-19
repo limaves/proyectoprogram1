3ee01c5012cc81fdb5f2f48b4fdb0f2e781c0ec4
unknown <libny.limas@ujcv.edu.hn>
2014-02-18 20:05:22 -0600

    termine el programa

--- src/proyecto1/proyecto1.cpp
+++ src/proyecto1/proyecto1.cpp
@@ -5,10 +5,15 @@
 
 using namespace std;
 int lanzarnumero();
-
+int obtenerlosnumerosiguales(int numeros[]);
 int main ()
 	
 {
+	char continua='s';
+
+	while(continua!='n')
+	{
+
 	
 	cout<<("lanza numeros ")<<endl;
 	cout<<("lanse el primer numero ")<<endl;
@@ -22,31 +27,61 @@ int main ()
 	 c = lanzarnumero();
 	 d = lanzarnumero();
     
+	 int numero [4] = {a, b, c, d};
 
+	 int numerosiguales=obtenerlosnumerosiguales(numero);
+
+	 cout<<"numerosiguales"<<numerosiguales<<endl;
+
+	 if(numerosiguales==4)
+		 cout<<"usted a ganado";
+	 if(numerosiguales==3)
+		 cout<<"usted a ganado medio punto";
+	 if(numerosiguales==2)
+		 cout<<"seguis jugando no gana nada";
+	 if(numerosiguales==0)
+		 cout<<"as perdido el juego";
 	 
-	
-	 if(a==b==c==d)
-	 {
-		 cout<<"usted a ganado :)";
-	 }
-	 else 
-	 {
-		 cout<<"usted a perdido :(";
-
-	 }
+	cout<<"desea continuar jugando s/n";
+	 cin>>continua;
 
 
-	getchar();
-	return 0;
+	}
+
+	 return 0;
 
 }
+int obtenerlosnumerosiguales(int numeros[])
+{
+	int conteodeiguales=0;
 
+	for (int a = 0; a <4; a++ )
+	{
+		int valor=numeros[a];
+
+	    int conteodeigualesinternos=0;
+
+		for(int b = 0; b <4; b++ )
+		{
+         if(a==b)break; 
+         int valor2=numeros[b];
+		 if( valor==valor2)conteodeigualesinternos++; 
+
+		}
+		if(conteodeigualesinternos>0)conteodeiguales=conteodeigualesinternos+1;
+
+	}
+	return conteodeiguales;
+
+}
 int lanzarnumero()  
 {	
 	int numero; 
 	numero=rand() % 9 + 0;
 	cout<<" el  numero es: "<<numero<<endl;
 	return numero;
+
+
 }
 
 
